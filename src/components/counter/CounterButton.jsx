6c166816e70e4e5d7e8d945a6f5c9e0e1e48c3da
Andy SanClemente <andysanclemente@gail.com>
2022-06-29 14:05:33 -0400

    Removed state from CounterButtons

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -7,33 +7,18 @@ class CounterButton extends Component {
     
     constructor() {
         super()
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
     }
 
 
     render() {
         return (
         <div >
-            <button className='counterButton' onClick={this.increment}>+{this.props.by}</button>
-            <button className='counterButton' onClick={this.decrement}>-{this.props.by}</button>
+            <button className='counterButton' onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+            <button className='counterButton' onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
         </div>
         )
     }
 
-    increment() {
-        // This references the property passed in
-        // when it's created in Counter
-        // incrementMethod={....}
-        this.props.incrementMethod(this.props.by);
-      }
-
-      decrement() {
-        // This references the property passed in
-        // when it's created in Counter
-        // incrementMethod={....}
-        this.props.decrementMethod(this.props.by);
-      }
   }
 
   CounterButton.defaultProps = {
