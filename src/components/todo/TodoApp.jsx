6499939bc6b217cb63f23af5e69e13b7b00de912
Andy SanClemente <andysanclemente@gail.com>
2022-06-30 10:02:47 -0400

    Unit 35 Step 6
    Login Screen basic validation with hardcode values

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, isValidElement} from "react";
 
 class TodoApp extends Component {
 
@@ -16,12 +16,55 @@ class TodoApp extends Component {
 }
 
 class LoginComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
+        }
+        // this.handleUserNameChange = this.handleUserNameChange.bind(this)
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name] : event.target.value})
+    }
+
+    loginClicked() {
+        if (this.state.username ==='ansancle' & this.state.password==='password') {
+            this.setState({showSuccessMessage:true})
+            this.setState({hasLoginFailed:false})
+        } else {
+            this.setState({showSuccessMessage:false})
+            this.setState({hasLoginFailed:true})
+        }
+    
+    }
+
+        
+    
+
+    // handleUserNameChange(event) {
+    //     this.setState({username : event.target.value})
+    // }
+
+    // handlePasswordChange(event) {
+    //     this.setState({password : event.target.value})
+    // }
+
     render() {
         return (
             <div className="LoginComponent">
-               User Name: <input type="text" name="username"/>
-               Password: <input type="password" name="password"/>
-               <button>Login</button>
+                <div name="invalid" hidden={!this.state.hasLoginFailed}>Invalid Credentials</div>
+                <div name="valid" hidden={!this.state.showSuccessMessage}>Login Sucessful</div>
+               User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+               Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+               <button onClick={this.loginClicked}>Login</button>
             </div>
         );
     }
@@ -30,3 +73,4 @@ class LoginComponent extends Component {
 
 
 export default TodoApp;
+
